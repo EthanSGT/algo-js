cbcff56a14a9c5707ff296d8f31406f31cba898c
EthanSGT <116520758+EthanSGT@users.noreply.github.com>
2023-10-10 10:01:58 +0200

    Add files via upload
    Ajout des exercices 4, 5 et d'un exercice bonnus

---
+++ ExBonnus/main.js
@@ -0,0 +1,10 @@
+let a = 4;
+let b = "4";
+let c = 3;
+
+if (a === b) {
+  /* "===" : permet de vérifier 2 éléments en valeur ET en type */
+  console.log("c bon");
+} else {
+  console.log("raté");
+}
