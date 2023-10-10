7df2a2e00870991b34705b3295de9991c494c8b3
EthanSGT <116520758+EthanSGT@users.noreply.github.com>
2023-10-10 11:12:09 +0200

    Ajout exercice 6

---
+++ Ex6/main.js
@@ -0,0 +1,20 @@
+let c = "Jean";
+let d = "Paul";
+
+function checkName(prenom1, prenom2) {
+  if (prenom1 == prenom2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+checkName(c, d);
+console.log(result);
+/*
+function checkName2(prenom1, prenom2) {
+  return prenom1==prenom2;
+}
+
+checkName2(c, d);
+console.log(result);*/
