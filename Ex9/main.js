a381dff995b739194440227b7cd16d68eea6df3f
EthanSGT <116520758+EthanSGT@users.noreply.github.com>
2023-10-13 09:48:19 +0200

    J'ai rajouter les parties qui étaient incomplète et incorrecte dans mon code

--- Ex9/main.js
+++ Ex9/main.js
@@ -8,25 +8,36 @@ class Pokemon {
   }
 
   getisLucky() {
-    return this.luck>Math.random();
+    return this.luck > Math.random();
   }
 
-  getattackPokemon() {
-    while (isLucky <= Pokemon.luck) {
-      console.log(`le Pokemon attaque avec ${this.attack} pts !`);
-      dégats = Pokemon.attack - Pokemon.defense;
-    }
+  getattackPokemon(pokemon) {
+    if (this.getisLucky()) {
+      let degat = Pokemon.attack - Pokemon.defense;
+      pokemon.hp -= degat;
+      //partie ajouté après correction :
+      console.log(this.name +'a attaqué' + pokemon.name+' pour '+ degat + 'de degats, il lui reste'+ pokemon.hp +' pv.');
+    } else{
+      console.log(this.name+' a raté son attaque !');
+    } //fin de la partie rajouté après correction
   }
 }
 
 let dracolosse = new Pokemon("Dracolosse", 10, 5, 20, 75);
 let carchacrok = new Pokemon("Carchacrok", 20, 3, 25, 50);
 
-
-while (dracolosse.hp > 0 && carchacrok.hp >0){
+while (dracolosse.hp > 0 && carchacrok.hp > 0) {
   dracolosse.getattackPokemon(carchacrok);
-} 
 
-if(carchacrok.hp<=0){
-    console.log(carchacrok.name +'est mort');
-}
+  if (carchacrok.hp <= 0) {
+    console.log(carchacrok.name + "est mort");
+    break;
+  }
+  // partie ajouté après correction
+  carchacrok.getattackPokemon(dracolosse);
+
+  if (dracolosse.hp <= 0) {
+    console.log(dracolosse.name + "est mort");
+    break;
+  }
+} //Fin de la partie ajouté après correction
